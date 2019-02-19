1228ad0436974e4b7f18a0e01a800207e1eedc32
Oleg Plaxin <plaxoleg@gmail.com>
2019-02-19 16:33:58 +0300

    changed test client_id to THE MARKET RUSH client_id, refactored code

--- vkToken.py
+++ vkToken.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 import re
 
+
 def getTokenFromWeb():
     driver = webdriver.Chrome(executable_path='driver/chromedriver.exe')
-    driver.get("https://vk.cc/8GIRRY")
+    driver.get("https://vk.cc/94gpA7")
     url = ''
     done = True
     while done:
         result = re.match(r'http://api.vk.com/blank', driver.current_url)
-        if result != None:
+        if result is not None:
             url = driver.current_url
             done = False
             driver.close()
-    token = url[re.search('access_token=', url).end() : re.search('&', url).start()]
+    token = url[re.search('access_token=', url).end(): re.search('&', url).start()]
     writeToken(token)
     return token
 
@@ -29,10 +30,12 @@ def readToken():
     f.close()
     return token
 
+
 def getToken():
     try:
         return readToken()
     except:
         return getTokenFromWeb()
+
 
 print(getToken())
